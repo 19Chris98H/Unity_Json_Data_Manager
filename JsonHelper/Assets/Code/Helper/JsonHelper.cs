1b8b3977a265c95fa4a74c69fa882e084b18e8af
Chris9898 <c.hoerath@gmx.net>
2023-05-26 23:40:38 +0200

    add json helper

---
+++ JsonHelper/Assets/Code/Helper/JsonHelper.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using UnityEngine;
+
+public static class JsonHelper
+{
+    public static void LoadJsonResource<T>(ref T destination, string path)
+    {
+        var textAsset = Resources.Load<TextAsset>(path);
+        if (textAsset)
+            destination = JsonConvert.DeserializeObject<T>(textAsset.text);
+        else
+            destination = (T)System.Activator.CreateInstance(typeof(T));
+    }
+    
+    public static void LoadJson<T>(ref T destination, string path)
+    {
+        if (File.Exists(path))
+            destination = JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
+        else
+            destination = (T)System.Activator.CreateInstance(typeof(T));
+    }
+    
+    public static void SaveJson<T>(T source, string path)
+    {
+        var directory = path.Remove(path.LastIndexOf('/') + 1);
+        if (!Directory.Exists(directory))
+            Directory.CreateDirectory(directory);
+
+        using (var file = File.CreateText(path))
+        {
+            var serializer = new JsonSerializer
+            {
+                Formatting = Formatting.Indented,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+            serializer.Serialize(file, source);
+        }
+    }
+}
